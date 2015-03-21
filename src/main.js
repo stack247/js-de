e18bfc2b4cb0eea9518072a549758b2f9a4ca521
suhendra.utet <suhendra.hui@outlook.com>
2015-03-21 16:55:40 -0500

    add folder

--- src/main.js
+++ src/main.js
@@ -24,15 +24,17 @@
  1. Create project root folder: 'js-de'.
 
  2. Initialize git repository: run 'git init'.
- 3. Add source control related files: ".gitignore", "README.md".
- 4. Add files to source control: run 'git add .'.
- 5. Add origin to source control: run ''.
+ 3. Add origin to source control: run 'git remote add origin https://github.com/stack247/js-de.git'.
+ 4. Add source control related files: ".gitignore", "README.md".
+ 5. Add files to source control: run 'git add .'.
+ 6. Commit to source control: run 'git commit -m "Initial commit"'.
+ 7. Push to source control: run 'git push origin master'.
 
- 1. Add folders (build, src, src/modules, src/test).
- 3. Add application related files: "index.html", "main.js", "greeting.js".
+ 8. Add folders (build, src, src/modules, src/test).
+ 9. Add application related files: "index.html", "main.js", "greeting.js".
 
- 5. Initialize npm package manager to create 'package.json': run 'npm init'.
- 4. Install 'Browserify' package globally: run 'npm install -g browserify'.
+ 10. Initialize npm package manager to create 'package.json': run 'npm init'.
+ 11. Install 'Browserify' package globally: run 'npm install -g browserify'.
  5. Install 'Browserify' in the project: run 'npm install -save browserify'.
  6. Install 'Gulp' package globally: Run 'npm install -g gulp'.
  7. Install 'Gulp' in the project: run 'npm install -save-dev gulp'.
