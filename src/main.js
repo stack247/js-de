1d51b08a0b893bb1727aee7d805f0f8e1d0a8958
suhendra.utet <suhendra.hui@outlook.com>
2015-03-21 13:15:49 -0500

    initial check in

--- src/main.js
+++ src/main.js
@@ -22,14 +22,21 @@
 
  Flow:
  1. Create project root folder: 'js-de'.
- 2. Initialize git repository.
+
+ 2. Initialize git repository: run 'git init'.
+ 3. Add source control related files: ".gitignore", "README.md".
+ 4. Add files to source control: run 'git add .'.
+ 5. Add origin to source control: run ''.
+
  1. Add folders (build, src, src/modules, src/test).
- 2. Add source control related files: ".gitignore", "README.md".
  3. Add application related files: "index.html", "main.js", "greeting.js".
- 4. Run 'npm install -g browserify'.
- 5. Run 'npm install -save browserify'.
- 6. Run 'npm install -g gulp'.
- 7. Run 'npm install -save-dev gulp'.
+
+ 5. Initialize npm package manager to create 'package.json': run 'npm init'.
+ 4. Install 'Browserify' package globally: run 'npm install -g browserify'.
+ 5. Install 'Browserify' in the project: run 'npm install -save browserify'.
+ 6. Install 'Gulp' package globally: Run 'npm install -g gulp'.
+ 7. Install 'Gulp' in the project: run 'npm install -save-dev gulp'.
+
  8. Add gulp task file: 'gulpfile.js'
 */
 
